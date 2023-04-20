366eeb6c139f30f1f4f1cf6a1709d228b0f9dd28
unknown <dch@uoregon.edu>
2023-04-19 17:35:54 -0700

    Extra Requirements Ghost

---
+++ Assets/UnityTechnologies/3DBeginnerTutorial/Scripts/Glider.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Glider : MonoBehaviour
+{
+    public List<GameObject> points = new List<GameObject>();
+
+    private Vector3 targetPosition = Vector3.zero;
+    private int index = 0;
+
+    public float speed = 2f;
+
+    private void Start()
+    {
+        if (points.Count < 2)
+        {
+            Debug.Log("Must assign glider 2+ points");
+            Destroy(gameObject);
+        }
+
+        index = (int)Mathf.Floor(Random.Range(0, points.Count));
+
+        targetPosition = points[index].transform.position;
+    }
+
+    private void Update()
+    {
+        Vector3 diff = targetPosition - transform.position;
+        float distance = diff.magnitude;
+        diff.Normalize();
+
+        float dotProduct = Vector3.Dot(transform.forward, diff);
+
+        if (dotProduct > 0.9f && distance < speed * Time.deltaTime)
+        {
+            transform.position = targetPosition;
+            int oldIndex = index;
+            while (oldIndex == index)
+                index = (int)Mathf.Floor(Random.Range(0, points.Count));
+            targetPosition = points[index].transform.position;
+        }
+        else
+        {
+            transform.position += diff * speed * Time.deltaTime;
+            transform.forward = Vector3.Lerp(transform.forward, diff, 0.1f);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -6,11 +6,6 @@ Aidan Duval - Player character
 Zach Weisenbloom - Audio/ending/Camera
 Brett Sumser - Enemies
 
-Dot product:
-...
-
-Linear interpolation:
-...
-
-Particle Effect:
-...
+The dot product, interpolation, and particle effect requirements were all fulfilled with a new ghost that glides around
+above the level. The dot product and interpolation were used to calculate the target position, the particle effect
+was used to create a trail effect.
